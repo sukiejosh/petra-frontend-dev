d025dd897af476836e69b3139998093e4ef8f2f3
joshua <oladokunjoshua2016@gmail.com>
2021-12-03 11:48:31 +0100

    fix PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import path from "path";
 import VueI18n from "@intlify/vite-plugin-vue-i18n";
 import Vue from "@vitejs/plugin-vue";
 import LinkAttributes from "markdown-it-link-attributes";
 import Prism from "markdown-it-prism";
+import path from "path";
 import AutoImport from "unplugin-auto-import/vite";
 import IconsResolver from "unplugin-icons/resolver";
 import Icons from "unplugin-icons/vite";
@@ -109,74 +109,74 @@ export default defineConfig({
         theme_color: "#ffffff",
         icons: [
           {
-            src: "./img/icons/android-chrome-192x192.png",
+            src: "./img/android-chrome-192x192.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "./img/icons/android-chrome-512x512.png",
+            src: "./img/android-chrome-512x512.png",
             sizes: "512x512",
             type: "image/png",
           },
           {
-            src: "./img/icons/android-chrome-maskable-192x192.png",
+            src: "./img/android-chrome-maskable-192x192.png",
             sizes: "192x192",
             type: "image/png",
             purpose: "maskable",
           },
           {
-            src: "./img/icons/android-chrome-maskable-512x512.png",
+            src: "./img/android-chrome-maskable-512x512.png",
             sizes: "512x512",
             type: "image/png",
             purpose: "maskable",
           },
           {
-            src: "./img/icons/apple-touch-icon-60x60.png",
+            src: "./img/apple-touch-icon-60x60.png",
             sizes: "60x60",
             type: "image/png",
           },
           {
-            src: "./img/icons/apple-touch-icon-76x76.png",
+            src: "./img/apple-touch-icon-76x76.png",
             sizes: "76x76",
             type: "image/png",
           },
           {
-            src: "./img/icons/apple-touch-icon-120x120.png",
+            src: "./img/apple-touch-icon-120x120.png",
             sizes: "120x120",
             type: "image/png",
           },
           {
-            src: "./img/icons/apple-touch-icon-152x152.png",
+            src: "./img/apple-touch-icon-152x152.png",
             sizes: "152x152",
             type: "image/png",
           },
           {
-            src: "./img/icons/apple-touch-icon-180x180.png",
+            src: "./img/apple-touch-icon-180x180.png",
             sizes: "180x180",
             type: "image/png",
           },
           {
-            src: "./img/icons/apple-touch-icon.png",
+            src: "./img/apple-touch-icon.png",
             sizes: "180x180",
             type: "image/png",
           },
           {
-            src: "./img/icons/favicon-16x16.png",
+            src: "./img/favicon-16x16.png",
             sizes: "16x16",
             type: "image/png",
           },
           {
-            src: "./img/icons/favicon-32x32.png",
+            src: "./img/favicon-32x32.png",
             sizes: "32x32",
             type: "image/png",
           },
           {
-            src: "./img/icons/msapplication-icon-144x144.png",
+            src: "./img/msapplication-icon-144x144.png",
             sizes: "144x144",
             type: "image/png",
           },
           {
-            src: "./img/icons/mstile-150x150.png",
+            src: "./img/mstile-150x150.png",
             sizes: "150x150",
             type: "image/png",
           },
